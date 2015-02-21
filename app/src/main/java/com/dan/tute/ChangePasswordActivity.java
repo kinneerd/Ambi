6b9644e5d93c4ba31041d78aa0a0ab81c0f8ff3b
briceridings <brice.m.ridings@gmail.com>
2015-02-20 21:25:35 -0500

    edited buttons

--- app/src/main/java/com/dan/tute/ChangePasswordActivity.java
+++ app/src/main/java/com/dan/tute/ChangePasswordActivity.java
@@ -1,6 +1,6 @@
 package com.dan.tute;
 
-import android.support.v7.app.ActionBarActivity;
+import  android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
