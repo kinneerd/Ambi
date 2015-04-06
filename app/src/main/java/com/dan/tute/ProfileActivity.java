e46c71fe9bbc3d58056c7d4551ba7ff6e4ad3777
briceridings <brice.m.ridings@gmail.com>
2015-04-06 14:07:28 -0400

    upgrade to tutor on interface

--- app/src/main/java/com/dan/tute/ProfileActivity.java
+++ app/src/main/java/com/dan/tute/ProfileActivity.java
@@ -1,15 +1,19 @@
 package com.dan.tute;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
